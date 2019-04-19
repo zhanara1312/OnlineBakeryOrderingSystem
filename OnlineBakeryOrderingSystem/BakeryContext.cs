6c860b69f705815cef0aa564c7d2699e5f154a93
Zhanara Ulan kyzy <Janarochka.juk@gmail.com>
2019-04-19 11:03:28 -0700

    EF

---
+++ OnlineBakeryOrderingSystem/BakeryContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineBakeryOrderingSystem
+{
+    class BakeryContext : DbContext
+    {
+        public DbSet<Bakery> Bakeries { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring
+            (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=OnlineBakeryOrderingSystem;Integrated Security=True;Connect Timeout=30;");
+        }
+
+        protected override void OnModelCreating
+            (ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Bakery>(entity =>
+            {
+                entity
+                    .HasKey(e => e.CustomerNumber)
+                    .HasName("PK_Bakeries");
+
+                entity.Property(e => e.CustomerNumber)
+                    .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.CustomerEmailAddress)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.BakeryProduct)
+                    .IsRequired();
+            });
+
+            modelBuilder.Entity<Transaction>(entity =>
+            {
+                entity.HasKey(e => e.TransactionId)
+                    .HasName("PK_Transactions");
+
+                entity.Property(e => e.TransactionId)
+                    .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.NumberOfOrder)
+                    .IsRequired();
+
+                entity.HasOne(e => e.Bakery)
+                    .WithMany()
+                    .HasForeignKey(e => e.CustomerNumber);
+
+            });
+        }
+
+    }
+}
---
+++ OnlineBakeryOrderingSystem/Migrations/20190419175937_Initial.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineBakeryOrderingSystem.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bakeries",
+                columns: table => new
+                {
+                    CustomerNumber = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BakeryProduct = table.Column<int>(nullable: false),
+                    ItemPrice = table.Column<decimal>(nullable: false),
+                    CustomerName = table.Column<string>(nullable: true),
+                    CustomerAddress = table.Column<string>(nullable: true),
+                    CustomerEmailAddress = table.Column<string>(maxLength: 50, nullable: false),
+                    CustomerBankAccountNumber = table.Column<string>(nullable: true),
+                    NumberOfOrder = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bakeries", x => x.CustomerNumber);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TransactionDate = table.Column<DateTime>(nullable: false),
+                    Itemprice = table.Column<decimal>(nullable: false),
+                    NumberOfOrder = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    TransactionType = table.Column<int>(nullable: false),
+                    CustomerNumber = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionId);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Bakeries_CustomerNumber",
+                        column: x => x.CustomerNumber,
+                        principalTable: "Bakeries",
+                        principalColumn: "CustomerNumber",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_CustomerNumber",
+                table: "Transactions",
+                column: "CustomerNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Bakeries");
+        }
+    }
+}
